250e7bf5367e8cffde50f16c7160932ca13e3b9a
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-05-24 15:24:21 +0200

    refactor: rename classes

--- src/elements/footer/footer.ts
+++ src/elements/footer/footer.ts
@@ -4,20 +4,22 @@ import './footer.scss';
 
 export default function footer() {
   const contact = Bootstrap.createElement('div', 'footerContact');
-  contact.append(createLink('Belarus, Minsk, Pobediteley Avenue 61', 'https://maps.app.goo.gl/EaNqAM4kFa8vqiFh8'));
-  contact.append(createLink('+375 (25) 512 33 42', 'tel:+375255123342'));
-  contact.append(createLink('Lithub-official@gmail.com', 'mailto:Lithub-official@gmail.com'));
+  contact.append(
+    createContactLink('Belarus, Minsk, Pobediteley Avenue 61', 'https://maps.app.goo.gl/EaNqAM4kFa8vqiFh8'),
+  );
+  contact.append(createContactLink('+375 (25) 512 33 42', 'tel:+375255123342'));
+  contact.append(createContactLink('Lithub-official@gmail.com', 'mailto:Lithub-official@gmail.com'));
 
-  const containver = Bootstrap.createElement('div', 'footer');
-  containver.append(createCart('Valeria110', 'https://github.com/Valeria110'));
-  containver.append(createCart('MikhailSemenuk', 'https://github.com/MikhailSemenuk'));
-  containver.append(createCart('qwgfsehte', 'https://github.com/qwgfsehte'));
+  const containver = Bootstrap.createElement('div', 'footerGithub');
+  containver.append(createGithubCart('Valeria110', 'https://github.com/Valeria110'));
+  containver.append(createGithubCart('MikhailSemenuk', 'https://github.com/MikhailSemenuk'));
+  containver.append(createGithubCart('qwgfsehte', 'https://github.com/qwgfsehte'));
 
   return [contact, containver];
 }
 
-function createCart(authorLogin: string, linkUrl: string) {
-  const link = Bootstrap.createElement('a', 'icon-link text-decoration-none footer__cart', authorLogin);
+function createGithubCart(authorLogin: string, linkUrl: string) {
+  const link = Bootstrap.createElement('a', 'icon-link text-decoration-none footerGithub__cart', authorLogin);
   link.href = linkUrl;
   link.target = '_blank';
   link.rel = 'noopener noreferrer';
@@ -29,7 +31,7 @@ function createCart(authorLogin: string, linkUrl: string) {
   return link;
 }
 
-function createLink(contactInfo: string, linkUrl: string) {
+function createContactLink(contactInfo: string, linkUrl: string) {
   const link = Bootstrap.createElement('a', 'text-decoration-none footerContact__cart', contactInfo);
   link.href = linkUrl;
   link.target = '_blank';
