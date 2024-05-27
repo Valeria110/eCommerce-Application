5f35e3671f69d698ba711e833235611f037edd5f
Valeria Ivashkevich <117567259+Valeria110@users.noreply.github.com>
2024-05-27 13:01:06 +0300

    feat: implement userProfilePage layout (#99)
    * feat: implement userProfilePage layout
    
    * feat: implement userProfilePage layout

--- src/elements/types.ts
+++ src/elements/types.ts
@@ -6,6 +6,7 @@ export enum Pages {
   Catalog = 'Catalog',
   AboutUS = 'About us',
   Product = 'Product',
+  UserProfile = 'UserProfile',
 }
 
 export enum AppEvents {
@@ -17,4 +18,54 @@ export interface Product {
   title: string;
   description: string;
   slug: string;
+}
+
+export interface IAddressObj {
+  id: string;
+  city: string;
+  country: string;
+  postalCode: string;
+  streetName: string;
+  streetNumber: string;
+}
+
+export interface IUserData {
+  id: string;
+  version: number;
+  versionModifiedAt: string;
+  lastMessageSequenceNumber: number;
+  createdAt: string;
+  lastModifiedAt: string;
+  lastModifiedBy: {
+    clientId: string;
+    isPlatformClient: boolean;
+  };
+  createdBy: {
+    clientId: string;
+    isPlatformClient: boolean;
+  };
+  email: string;
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  password: string;
+  addresses: {
+    id: string;
+    title: string;
+    firstName: string;
+    lastName: string;
+    streetName: string;
+    streetNumber: string;
+    postalCode: string;
+    city: string;
+    country: string;
+    email: string;
+  }[];
+  defaultShippingAddressId: string;
+  defaultBillingAddressId: string;
+  shippingAddressIds: string[];
+  billingAddressIds: string[];
+  isEmailVerified: boolean;
+  stores: [];
+  authenticationMode: string;
 }
