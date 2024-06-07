fb6607828ee98585869e1622ae10f27235d1cb3c
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-06-07 17:33:21 +0200

    refactor: del comment

--- src/pages/productPage/product.ts
+++ src/pages/productPage/product.ts
@@ -122,7 +122,6 @@ function createRightColumn(response: Product) {
   const buyBtn = Bootstrap.createButton('Buy', 'btn-orange border-0 m-1 product__btn product__btnBuy');
   const addCartBtn = Bootstrap.createButton('Add to card', 'btn-white m-1 product__btn product__btnAddToCard');
   addCartBtn.addEventListener('click', () => {
-    console.log('try add product to cart');
     cart.addProduct(response.id);
   });
   const wrapperBtn = Bootstrap.createElement('div');
