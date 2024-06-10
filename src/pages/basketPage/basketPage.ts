9892b188568441fe65b86992d1f5763a77cf55d6
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-06-10 13:41:07 +0200

    feat: summary in basket

--- src/pages/basketPage/basketPage.ts
+++ src/pages/basketPage/basketPage.ts
@@ -18,8 +18,59 @@ export default function basketPage() {
     });
   });
 
-  container.append(cartTempControlPanel(), generateBtn, products);
+  const productSummary = Bootstrap.createElement('div', 'd-flex justify-content-between');
+  productSummary.append(products, createSummary());
 
+  container.append(cartTempControlPanel(), generateBtn, productSummary);
+
+  return container;
+}
+
+function createSummary() {
+  const container = Bootstrap.createElement('div', 'basketSummary');
+
+  const title = Bootstrap.createElement('h2', 'basketSummary__title', 'Summary');
+
+  const titlePromo = Bootstrap.createElement('h4', 'basketSummary__titlePromo', 'Promocode');
+
+  const promoGroup = Bootstrap.createElement('div', 'input-group mb-3');
+  const promoGroupId = 'basketPromoInput';
+
+  const promoInput = Bootstrap.createElement('input', 'form-control');
+  promoInput.setAttribute('type', 'text');
+  promoInput.setAttribute('placeholder', 'Promocode');
+  promoInput.setAttribute('aria-label', 'Promocode');
+  promoInput.setAttribute('aria-describedby', promoGroupId);
+
+  const promoButton = Bootstrap.createElement('button', 'btn btn-orange basketSummary__apply', 'Apply');
+  promoButton.setAttribute('type', 'button');
+  promoButton.setAttribute('id', promoGroupId);
+
+  promoGroup.append(promoInput, promoButton);
+
+  const checkOutBtn = Bootstrap.createButton('Check out', 'btn-orange border-0 basketSummary__btnCheckOut');
+
+  const createPriceLine = (
+    text: string,
+    price: string,
+    style: 'basketSummary__boldLine' | 'basketSummary__grayLine',
+  ) => {
+    const line1 = Bootstrap.createElement('div', 'd-flex justify-content-between');
+    line1.classList.add(style);
+    const text1 = Bootstrap.createElement('span', '', text);
+    const price1 = Bootstrap.createElement('span', '', price);
+    line1.append(text1, price1);
+    return line1;
+  };
+
+  const line1 = createPriceLine('The amount without discount', '52$', 'basketSummary__grayLine');
+  const line2 = createPriceLine('Discount', '0$', 'basketSummary__grayLine');
+  const line3 = createPriceLine('Promocode', '5$', 'basketSummary__grayLine');
+  const line4 = createPriceLine('Total', '47$', 'basketSummary__boldLine');
+  const lines = Bootstrap.createElement('div', 'basketSummary__linesWrapper');
+  lines.append(line1, line2, line3, line4);
+
+  container.append(title, titlePromo, promoGroup, lines, checkOutBtn);
   return container;
 }
 
